23dbd13b4b21beddf5e13c742261ee098f33ecd6
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:21 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedHeader.bootstrap4.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-bs4";import DataTable from"datatables.net-fixedheader";export default DataTable;
---
+++ js/fixedHeader.bootstrap4.mjs
@@ -0,0 +1,13 @@
+
+/*! Bootstrap 4 styling wrapper for FixedHeader
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs4';
+import DataTable from 'datatables.net-fixedheader';
+
+
+
+
+export default DataTable;
