c0357d4ce59eb253e9d300a19f7eebef62834553
dtbuild <dtbuild@rigil>
2026-05-07 13:47:42 +0000

    7cf0b29dd3d805e373e6cd1aad5efbc6fffbf1dd
    Sync to source repo @bb639877e328dd51de2a396ecc51e574e100846c

--- js/fixedHeader.bootstrap4.min.mjs
+++ js/fixedHeader.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! Bootstrap 4 styling wrapper for FixedHeader
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedHeader Bootstrap 4 styling 5.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-bs4";import FixedHeader from"datatables.net-fixedheader";let $=jQuery;export default DataTable;
+import DataTable from"datatables.net-bs4";import FixedHeader from"datatables.net-fixedheader";export default DataTable;
--- js/fixedHeader.bootstrap4.mjs
+++ js/fixedHeader.bootstrap4.mjs
@@ -1,14 +1,11 @@
-/*! Bootstrap 4 styling wrapper for FixedHeader
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedHeader Bootstrap 4 styling 5.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs4';
 import FixedHeader from 'datatables.net-fixedheader';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
