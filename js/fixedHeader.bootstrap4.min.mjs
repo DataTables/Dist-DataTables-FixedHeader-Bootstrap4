4e04eed5d851566a067a81d5e68f2375efadcc7a
dtbuild <dtbuild@rigil>
2026-07-22 14:35:46 +0000

    8d8bc3e5051da6265f75030508feed1fde2a91ff Release 5.0.0 ready
    Sync to source repo @8d8bc3e5051da6265f75030508feed1fde2a91ff

--- js/fixedHeader.bootstrap4.min.mjs
+++ js/fixedHeader.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! FixedHeader Bootstrap 4 styling 5.0.0-beta.1 for DataTables
+/*! FixedHeader Bootstrap 4 styling 5.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import FixedHeader from"datatables.net-fixedheader";export default DataTable;
